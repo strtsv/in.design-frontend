b90ab69e6dfecb8363d777483a399ea96500eb4b
tenoclock <enr@bk.ru>
2024-02-28 03:22:35 +0500

    home page

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Loading from "../components/Home/Loading";
+import Content from "../components/Home/Content";
+
+const Home = () => {
+  return (
+    <>
+      <Loading />
+      <Content />
+    </>
+  );
+};
+
+export default Home;
